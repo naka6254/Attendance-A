b0d0540a9869e70dc38072d107c5847fb7954bc8
naka6254 <shomonike1221@yahoo.co.jp>
2022-06-21 12:16:27 +0000

    8.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -11,11 +12,25 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
-    render :new
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit      
     end
   end
 
@@ -23,5 +38,15 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeフィルター
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
     end
 end
